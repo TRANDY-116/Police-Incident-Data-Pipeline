1dc1f42fac1e041c3f7ea49faa31e14159131294
TRANDY-116 <randyfun16@gmail.com>
2024-11-11 13:53:50 +0100

    ediitign the agg incident trends script

--- dbt/Police_analytics/models/marts/police/agg_incident_trends.sql
+++ dbt/Police_analytics/models/marts/police/agg_incident_trends.sql
@@ -2,7 +2,13 @@ with monthly_counts as (
     select
         date_trunc('month', incident_datetime) as incident_month,
         incident_category,
-        count(*) as incident_count
+        count(*) as incident_count,
+        incident_day_of_week
+        case 
+            when extract(month from incident_datetime) in (6, 7, 8) then 'Summer'
+            when extract(month from incident_datetime) in (12, 1, 2) then 'Winter'
+            else 'Other' 
+        end as season
     from {{ ref('stg_police_incidents') }}
     group by 1, 2
     order by 1, 2
