577364de4647ce6aab7ebe1caeea6f05e4c288ff
TRANDY-116 <randyfun16@gmail.com>
2024-11-11 05:46:25 +0100

    dim_dates table created

---
+++ dbt/Police_analytics/models/example/dim_dates.sql
@@ -0,0 +1,13 @@
+with dates as (
+    select generate_series(
+        '2015-01-01'::date, '2025-12-31'::date, interval '1 day'
+    ) as date
+)
+select
+    date as full_date,
+    extract(year from date) as year,
+    extract(month from date) as month,
+    extract(day from date) as day,
+    extract(dow from date) as day_of_week,
+    extract(quarter from date) as quarter
+from dates
