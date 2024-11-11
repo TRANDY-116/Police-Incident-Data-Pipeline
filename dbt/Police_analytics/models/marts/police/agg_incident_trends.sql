7a7fab9ae61638392dfd47be12dc99609930068b
TRANDY-116 <randyfun16@gmail.com>
2024-11-11 13:54:22 +0100

    forgot and ,  in the code

--- dbt/Police_analytics/models/marts/police/agg_incident_trends.sql
+++ dbt/Police_analytics/models/marts/police/agg_incident_trends.sql
@@ -3,7 +3,7 @@ with monthly_counts as (
         date_trunc('month', incident_datetime) as incident_month,
         incident_category,
         count(*) as incident_count,
-        incident_day_of_week
+        incident_day_of_week,
         case 
             when extract(month from incident_datetime) in (6, 7, 8) then 'Summer'
             when extract(month from incident_datetime) in (12, 1, 2) then 'Winter'
