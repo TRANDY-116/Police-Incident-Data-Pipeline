b34a652d3524de4838733a9460a2ef4cd4c460cf
TRANDY-116 <randyfun16@gmail.com>
2024-11-11 05:27:52 +0100

    editing dim location

--- dbt/Police_analytics/models/example/dim_location.sql
+++ dbt/Police_analytics/models/example/dim_location.sql
@@ -6,4 +6,63 @@ select distinct
     longitude
 from {{ source('police_data', 'police_2018') }}
 
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2019') }}
+
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2020') }}
+
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2021') }}
+
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2022') }}
+
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2023') }}
+
+union all
+
+select distinct
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude
+from {{ source('police_data', 'police_2024') }}
 
