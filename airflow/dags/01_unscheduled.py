4203f197de241bc974311137314eafd8b544adff
TRANDY-116 <randyfun16@gmail.com>
2025-10-10 11:40:01 +0100

    at learning sporatic event schdeuleing on airflow

---
+++ airflow/dags/01_unscheduled.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pandas as pd
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+
+
+def _calculate_stats(input_path, output_path):
+    """Calculates event statistics."""
+
+    events = pd.read_json(input_path, convert_dates=["timestamp"])
+
+    stats = (
+        events.assign(date=lambda df: df["timestamp"].dt.date).groupby(["date", "user"]).size().reset_index()
+    )
+
+    Path(output_path).parent.mkdir(exist_ok=True)
+    stats.to_csv(output_path, index=False)
+
+
+with DAG(
+    dag_id="01_unscheduled",
+    schedule=None,
+):
+    fetch_events = BashOperator(
+        task_id="fetch_events",
+        bash_command=(
+            "sudo mkdir -p /data/01_unscheduled && "
+            "curl -o /data/01_unscheduled/events.json "
+            "http://events-api:8081/events/latest"
+        ),
+    )
+
+    calculate_stats = PythonOperator(
+        task_id="calculate_stats",
+        python_callable=_calculate_stats,
+        op_kwargs={
+            "input_path": "/data/01_unscheduled/events.json",
+            "output_path": "/data/01_unscheduled/stats.csv",
+        },
+    )
+
+    fetch_events >> calculate_stats
